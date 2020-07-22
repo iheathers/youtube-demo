fc03646b9576ca8a00a0e9ab3ed8c8c68c0ac3ed
iheathers <shresthapritam7@gmail.com>
2020-07-22 11:30:53 +0545

    Updates

---
+++ precache-manifest.4e567e2ca4f6c137e8f440b57df17b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650a5e33950c59c959b574a8a84c0868",
+    "url": "/youtube-demo/index.html"
+  },
+  {
+    "revision": "ba2e28e7e385fe70ce71",
+    "url": "/youtube-demo/static/css/main.04818bbb.chunk.css"
+  },
+  {
+    "revision": "aeaad422200a60eebb5d",
+    "url": "/youtube-demo/static/js/2.df0e160c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-demo/static/js/2.df0e160c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2e28e7e385fe70ce71",
+    "url": "/youtube-demo/static/js/main.4e8cd364.chunk.js"
+  },
+  {
+    "revision": "134fd3fe4131cb9e81af",
+    "url": "/youtube-demo/static/js/runtime-main.165dfcde.js"
+  }
+]);
